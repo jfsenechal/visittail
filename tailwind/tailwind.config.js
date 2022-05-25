61078d79ee9fe8ac6c19dc12e59f50d074780eb6
jfsenechal <jfsenechal@gmail.com>
2022-05-25 16:57:53 +0200

    header article

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -1,21 +1,54 @@
 // Set flag to include Preflight conditionally based on the build target.
-const includePreflight = ( 'editor' === process.env._TW_TARGET ) ? false : true;
+const includePreflight = "editor" === process.env._TW_TARGET ? false : true;
 
 module.exports = {
 	presets: [
 		// Manage Tailwind Typography's configuration in a separate file.
-		require( './tailwind-typography.config.js' ),
+		require("./tailwind-typography.config.js"),
 	],
 	content: [
 		// Ensure changes to PHP files and `theme.json` trigger a rebuild.
-		'./theme/**/*.php',
-		'./theme/templates/**/*.twig',
-		'./theme/theme.json',
+		"./theme/**/*.php",
+		"./theme/templates/**/*.twig",
+		"./theme/theme.json",
 	],
 	theme: {
 		// Extend the default Tailwind theme.
 		extend: {
-
+			fontFamily: {
+				sans: ["Monserrat"],
+			},
+			colors: {
+				cta: {
+					light: "#fd8383",
+					dark: "#487F89FF",
+					hover: "#FD83838C",
+				},
+				patrimony: "#AAB7D8FF",
+				walk: "#64966FFF",
+				art: "#F5CC73FF",
+				delicacy: "#EFBFB1FF",
+				party: "#EFD7CDFF",
+				home: "#E8DACBFF",
+				caractere: "#636061",
+				borderjf: "#dee2e6",
+				grey: {
+					dark: "#636061FF",
+					basic: "#808080FF",
+				},
+				bglighter: "#ededec",
+			},
+			backgroundImage: {
+				bg_home:
+					"url('https://visitmarche.be/wp-content/themes/visitmarche/assets/tartine/rsc/img/bg_home.jpg')",
+				chasse:
+					"url('https://visitmarche.be/wp-content/uploads/2021/09/DSC_0504-scaled.jpg')",
+				escape:
+					"url('https://visitmarche.be/wp-content/uploads/2021/10/4index.png')",
+				noel: "url('https://visitmarche.be/wp-content/uploads/2021/11/christmas-g22147f560_1280.png')",
+				totemus:
+					"url('https://visitmarche.be/wp-content/uploads/2021/05/Chasse-au-tre%E2%95%A0usor.jpeg')",
+			},
 		},
 	},
 	corePlugins: {
@@ -24,10 +57,10 @@ module.exports = {
 	},
 	plugins: [
 		// Add Tailwind Typography.
-		require( '@tailwindcss/typography' ),
+		require("@tailwindcss/typography"),
 
 		// Extract colors and widths from `theme.json`.
-		require( '@_tw/themejson' )( require( '../theme/theme.json' ) ),
+		require("@_tw/themejson")(require("../theme/theme.json")),
 
 		// Uncomment below to add additional first-party Tailwind plugins.
 		// require( '@tailwindcss/aspect-ratio' ),
--- theme/functions.php
+++ theme/functions.php
@@ -180,3 +180,9 @@ require get_template_directory() . '/inc/template-tags.php';
  * Functions which enhance the theme by hooking into WordPress.
  */
 require get_template_directory() . '/inc/template-functions.php';
+
+        // REMOVE WP EMOJI
+        remove_action('wp_head', 'print_emoji_detection_script', 7);
+        remove_action('wp_print_styles', 'print_emoji_styles');
+        remove_action('admin_print_scripts', 'print_emoji_detection_script');
+        remove_action('admin_print_styles', 'print_emoji_styles');
